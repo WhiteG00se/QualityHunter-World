c5eabeda981d1b1e5110b12da2d7bfcf7dc418b3
White Goose <t.lauppe@outlook.com>
2024-03-22 00:19:41 +0100

    Patch 2.0
    Patch 2.0

--- src/common-equip-armor.am_dat.py
+++ src/common-equip-armor.am_dat.py
@@ -15,7 +15,7 @@ def write_json_file(data, output_file):
 
 def modify_entry(data):
     for entry in data["Entries"]:
-        entry["Defense (ushort)"] = math.ceil(entry["Defense (ushort)"] * 1.75)
+        # entry["Defense (ushort)"] = math.ceil(entry["Defense (ushort)"] * 1.75)
         entry["Fire Res (byte)"] = max(0, entry["Fire Res (byte)"])
         entry["Water Res (byte)"] = max(0, entry["Water Res (byte)"])
         entry["Ice Res (byte)"] = max(0, entry["Ice Res (byte)"])
@@ -24,14 +24,21 @@ def modify_entry(data):
         if entry["Equip Slot (ubyte)"] == 5 and entry["Skill 1 (ushort)"] != 0:
             if entry["Skill 1 (ushort)"] != 104: # 104: Flinch Free
                 if entry["Skill 2 (ushort)"] == 0:
-                    entry["Skill 2 (ushort)"] = 104  # 104: Flinch Free
+                    entry["Skill 2 (ushort)"] = 104
                     entry["Skill 2 Level (ubyte)"] = 1
                 else:
-                    entry["Skill 3 (ushort)"] = 104  # 104: Flinch Free
+                    entry["Skill 3 (ushort)"] = 104
                     entry["Skill 3 Level (ubyte)"] = 1
             else:
                 entry["Skill 2 (ushort)"] = 97  # 97: Divine Blessing
                 entry["Skill 2 Level (ubyte)"] = 1
+        if entry["Defense (ushort)"] >= 1:
+            if entry["Skill 1 (ushort)"] == 23: # 23: Recovery Speed
+                entry["Skill 1 (ushort)"] = 22 # 22: Recovery Up
+            elif entry["Skill 2 (ushort)"] == 23:
+                entry["Skill 2 (ushort)"] = 22
+            elif entry["Skill 3 (ushort)"] == 23:
+                entry["Skill 3 (ushort)"] = 22
                 
 
 data = parse_json_file(inputFile)
