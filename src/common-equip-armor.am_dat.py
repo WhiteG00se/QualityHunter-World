8903d241142cafaceab9f16fddd06d9fbda7cae3
White Goose <t.lauppe@outlook.com>
2024-03-27 22:54:41 +0100

    Patch 2.8
    Patch 2.8

--- src/common-equip-armor.am_dat.py
+++ src/common-equip-armor.am_dat.py
@@ -15,7 +15,7 @@ def write_json_file(data, output_file):
 
 def modify_entry(data):
     for entry in data["Entries"]:
-        entry["Defense (ushort)"] = math.ceil(entry["Defense (ushort)"] * 1.15)
+        entry["Defense (ushort)"] = math.ceil(entry["Defense (ushort)"] * 1.05)
         entry["Fire Res (byte)"] = max(0, entry["Fire Res (byte)"])
         entry["Water Res (byte)"] = max(0, entry["Water Res (byte)"])
         entry["Ice Res (byte)"] = max(0, entry["Ice Res (byte)"])
@@ -28,6 +28,12 @@ def modify_entry(data):
                 entry["Skill 2 (ushort)"] = 22
             elif entry["Skill 3 (ushort)"] == 23:
                 entry["Skill 3 (ushort)"] = 22
+            if entry["Skill 1 (ushort)"] == 86: # 86: Speed Sharpening
+                entry["Skill 1 (ushort)"] = 85 # 85: Speed Eating
+            elif entry["Skill 2 (ushort)"] == 86:
+                entry["Skill 2 (ushort)"] = 85
+            elif entry["Skill 3 (ushort)"] == 86:
+                entry["Skill 3 (ushort)"] = 85
         elif entry["Equip Slot (ubyte)"] == 5 and entry["Skill 1 (ushort)"] != 0: #if charm
             if entry["Skill 1 (ushort)"] != 104: # 104: Flinch Free
                 if entry["Skill 2 (ushort)"] == 0:
